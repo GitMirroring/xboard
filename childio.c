9c3feeb64418d5c2d3a8896d289fe50390509d87
Dave Gomboc <dave_gomboc@acm.org>
2026-02-27 01:29:32 -0800

    Have childio.c contain more of its C code directly.
    We now only rely upon the build system to determine which particular
    pre-defined macro will be used for the actual definitions of
    PTY_ITERATION, PTY_NAME_C_STR, and PTY_TTY_NAME_C_STR.  All of the
    pre-defined macros that should be used for these are now already
    located within childio.c itself.
    
    Setting things up this way makes it easier to understand the
    PTY-handling code in childio.c without crossreferencing configure.ac
    and other parts of the build system.  Furthermore, because, the
    specific C code is no longer made visible to the macro processing that
    is done by the build system, we are assured that said C code isn't
    being corrupted by any of that processing.
    
    Amusingly, I believe that I was actually the person who reported the
    issue flagged here by IBM_RT_PC_AIX to Tim Mann circa 2003, and who
    helped run tests for attempted fixes, back when I was using some of
    those machines at the University of Alberta.

--- childio.c
+++ childio.c
@@ -4,9 +4,7 @@
  * Copyright 1991 by Digital Equipment Corporation, Maynard,
  * Massachusetts.
  *
- * Enhancements Copyright 1992-2001, 2002, 2003, 2004, 2005, 2006,
- * 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 Free
- * Software Foundation, Inc.
+ * Enhancements Copyright 1992-2016, 2026 Free Software Foundation, Inc.
  *
  * The following terms apply to Digital Equipment Corporation's copyright
  * interest in XBoard:
@@ -98,11 +96,11 @@ SetUpChildIO (int to_prog[2], int from_prog[2])
 #endif /* HAVE_STROPTS_H */
 #if HAVE_SYS_FCNTL_H
 # include <sys/fcntl.h>
-#else /* not HAVE_SYS_FCNTL_H */
+#else /* do not HAVE_SYS_FCNTL_H */
 # if HAVE_FCNTL_H
 #  include <fcntl.h>
 # endif /* HAVE_FCNTL_H */
-#endif /* not HAVE_SYS_FCNTL_H */
+#endif /* do not HAVE_SYS_FCNTL_H */
 
 int
 PseudoTTY (char pty_name[]);
@@ -113,23 +111,22 @@ SetUpChildIO (int to_prog[2], int from_prog[2])
     char pty_name[MSG_SIZ];
 
     if ((to_prog[1] = PseudoTTY(pty_name)) == -1) {
-	DisplayFatalError("Can't open pseudo-tty", errno, 1);
-	ExitEvent(1);
+        DisplayFatalError("Can't open pseudo-tty", errno, 1);
+        ExitEvent(1);
     }
     from_prog[0] = to_prog[1];
     to_prog[0] = from_prog[1] = open(pty_name, O_RDWR, 0);
 
 #if HAVE_STROPTS_H /* do we really need this??  pipe-like behavior is fine */
     if (ioctl (to_prog[0], I_PUSH, "ptem") == -1 ||
-	ioctl (to_prog[0], I_PUSH, "ldterm") == -1 ||
-	ioctl (to_prog[0], I_PUSH, "ttcompat") == -1) {
+        ioctl (to_prog[0], I_PUSH, "ldterm") == -1 ||
+        ioctl (to_prog[0], I_PUSH, "ttcompat") == -1) {
 # ifdef NOTDEF /* seems some systems don't have or need ptem and ttcompat */
-	DisplayFatalError("Can't ioctl pseudo-tty", errno, 1);
-	ExitEvent(1);
-# endif /*NOTDEF*/
+        DisplayFatalError("Can't ioctl pseudo-tty", errno, 1);
+        ExitEvent(1);
+# endif /* NOTDEF */
     }
 #endif /* HAVE_STROPTS_H */
-
 }
 
 #if HAVE_GRANTPT
@@ -150,8 +147,7 @@ PseudoTTY (char pty_name[])
     return fd;
 }
 
-#else /* not HAVE_GRANTPT */
-#if HAVE__GETPTY
+#elif HAVE__GETPTY
 /* This code is for IRIX */
 
 int
@@ -166,8 +162,7 @@ PseudoTTY (char pty_name[])
     return fd;
 }
 
-#else /* not HAVE__GETPTY */
-#if HAVE_LIBSEQ
+#elif HAVE_LIBSEQ
 /* This code is for Sequent DYNIX/ptx.  Untested. --tpm */
 
 int
@@ -178,94 +173,115 @@ PseudoTTY (char pty_name[])
 
     fd = getpseudotty(&slave, &master);
     if (fd < 0) return fd;
-    safeStrCpy(pty_name, slave, sizeof(pty_name)/sizeof(pty_name[0]));
+    safeStrCpy(pty_name, slave, sizeof(pty_name));
     return fd;
 }
 
-#else /* not HAVE_LIBSEQ */
+#else
 /* This code is for all other systems */
 /* The code is adapted from GNU Emacs 19.24 */
 
 #ifndef FIRST_PTY_LETTER
-#define FIRST_PTY_LETTER 'p'
+# define FIRST_PTY_LETTER 'p'
 #endif
 #ifndef LAST_PTY_LETTER
-#define LAST_PTY_LETTER 'z'
+# define LAST_PTY_LETTER 'z'
+#endif
+
+#define DEFAULT_PTY_ITERATION for (ch = FIRST_PTY_LETTER; ch <= LAST_PTY_LETTER; ++ch) for (i = 0; i < 16; ++i)
+#define DEFAULT_PTY_NAME_C_STR sprintf(pty_name, "/dev/pty%c%x", ch, i)
+#define DEFAULT_PTY_TTY_NAME_C_STR sprintf(pty_name, "/dev/tty%c%x", ch, i)
+
+#define HPUX_PTY_NAME_C_STR sprintf(pty_name, "/dev/ptym/pty%c%x", ch, i)
+#define HPUX_PTY_TTY_NAME_C_STR sprintf(pty_name, "/dev/pty/tty%c%x", ch, i)
+
+#define AIX3_BOSX_PTY_ITERATION for (ch = 0; !ch; ++ch)
+#define AIX3_BOSX_PTY_NAME_C_STR strcpy(pty_name, "/dev/ptc")
+#define AIX3_BOSX_PTY_TTY_NAME_C_STR strcpy(pty_name, ttyname(fd))
+
+#define RTU_PTY_NAME_C_STR sprintf(pty_name, "/dev/pty%x", i)
+#define RTU_PTY_TTY_NAME_C_STR sprintf(pty_name, "/dev/ttyp%x", i)
+
+#define IRIS_IRIX3_PTY_ITERATION for (ch = 0; !ch; ++ch)
+#define IRIS_IRIX3_PTY_NAME_C_STR strcpy(pty_name, "/dev/ptc")
+#define IRIS_IRIX3_PTY_TTY_NAME_C_STR sprintf(pty_name, "/dev/ttyq%d", minor(stb.st_rdev))
+
+#define SCO_PTY_ITERATION for (i = 0; ; ++i)
+#define SCO_PTY_NAME_C_STR sprintf(pty_name, "/dev/ptyp%d", i)
+#define SCO_PTY_TTY_NAME_C_STR sprintf(pty_name, "/dev/ttyp%d", i)
+
+/* The build system will augment the compilation flags such that PTY_ITERATION
+   is #defined to the appropriate *_PTY_ITERATION from the options above. */
+#ifndef PTY_ITERATION
+# error "PTY_ITERATION ought to have been defined."
+"PTY_ITERATION ought to have been defined."
+#endif
+
+/* The build system will augment the compilation flags such that PTY_NAME_C_STR
+   is #defined to the appropriate *_PTY_NAME_C_STR from the options above. */
+#ifndef PTY_NAME_C_STR
+# error "PTY_NAME_C_STR ought to have been defined."
+"PTY_NAME_C_STR ought to have been defined."
+#endif
+
+/* The build system will augment the compilation flags such that
+   PTY_TTY_NAME_C_STR is #defined to the appropriate *_PTY_TTY_NAME_C_STR from
+   the options above. */
+#ifndef PTY_TTY_NAME_C_STR
+# error "PTY_TTY_NAME_C_STR ought to have been defined."
+"PTY_TTY_NAME_C_STR ought to have been defined."
 #endif
 
 int
 PseudoTTY (char pty_name[])
 {
-  struct stat stb;
-  register c, i;
-  int fd;
+    struct stat stb;
+    char ch;
+    int fd;
+    int i;
 
-  /* Some systems name their pseudoterminals so that there are gaps in
-     the usual sequence - for example, on HP9000/S700 systems, there
-     are no pseudoterminals with names ending in 'f'.  So we wait for
-     three failures in a row before deciding that we've reached the
-     end of the ptys.  */
-  int failed_count = 0;
+    /* Some systems name their pseudoterminals so that there are gaps
+       in the usual sequence.  For example, on HP9000/S700 systems,
+       there are no pseudoterminals with names ending in 'f'.  So, we
+       wait for three failures in a row before deciding that we've
+       reached the end of the ptys. */
+    int consecutive_failures_count = 0;
 
-#ifdef PTY_ITERATION
-  PTY_ITERATION
-#else
-  for (c = FIRST_PTY_LETTER; c <= LAST_PTY_LETTER; c++)
-    for (i = 0; i < 16; i++)
-#endif
-      {
-#ifdef PTY_NAME_SPRINTF
-	PTY_NAME_SPRINTF
-#else
-	  sprintf (pty_name, "/dev/pty%c%x", c, i);
-#endif /* no PTY_NAME_SPRINTF */
+    PTY_ITERATION {
+        PTY_NAME_C_STR;
+        if (stat (pty_name, &stb) < 0) {
+            ++consecutive_failures_count;
+            if (consecutive_failures_count == 3) {
+                return -1;
+            }
+        } else {
+            consecutive_failures_count = 0;
+        }
+        fd = open (pty_name, O_RDWR, 0);
 
-#ifdef PTY_OPEN
-	PTY_OPEN;
-#else /* no PTY_OPEN */
-	if (stat (pty_name, &stb) < 0)
-	  {
-	    failed_count++;
-	    if (failed_count >= 3)
-	      return -1;
-	  }
-	else
-	  failed_count = 0;
-	fd = open (pty_name, O_RDWR, 0);
-#endif /* no PTY_OPEN */
-
-	if (fd >= 0)
-	  {
-	    /* check to make certain that both sides are available
-	       this avoids a nasty yet stupid bug in rlogins */
-#ifdef PTY_TTY_NAME_SPRINTF
-	    PTY_TTY_NAME_SPRINTF
-#else
-	      sprintf (pty_name,  "/dev/tty%c%x", c, i);
-#endif /* no PTY_TTY_NAME_SPRINTF */
+        if (fd >= 0) {
+            /* check to make certain that both sides are available
+               this avoids a nasty yet stupid bug in rlogins */
+            PTY_TTY_NAME_C_STR;
 #ifndef UNIPLUS
-	    if (access (pty_name, 6) != 0)
-	      {
-		close (fd);
-		continue;
-	      }
-#endif /* not UNIPLUS */
-#ifdef IBMRTAIX
-	      /* On AIX, the parent gets SIGHUP when a pty attached
-                 child dies.  So, we ignore SIGHUP once we've started
-                 a child on a pty.  Note that this may cause xboard
-                 not to die when it should, i.e., when its own
-                 controlling tty goes away.
-	      */
-	      signal(SIGHUP, SIG_IGN);
-#endif /* IBMRTAIX */
-	    return fd;
-	  }
-      }
-  return -1;
+            if (access (pty_name, 6) != 0) {
+                close (fd);
+                continue;
+            }
+#endif /* ifndef UNIPLUS */
+#ifdef IBM_RT_PC_AIX
+            /* On these systems, the parent gets SIGHUP when a
+               pty-attached child dies.  So, we ignore SIGHUP once
+               we've started a child on a pty.  Note that this may
+               cause XBoard not to die when it should, i.e., when its
+               own controlling tty goes away. */
+            signal(SIGHUP, SIG_IGN);
+#endif /* IBM_RT_PC_AIX */
+            return fd;
+        }
+    }
+    return -1;
 }
 
-#endif /* not HAVE_LIBSEQ */
-#endif /* not HAVE__GETPTY */
-#endif /* not HAVE_GRANTPT */
+#endif /* HAVE_GRANTPT #elif HAVE__GETPTY #elif HAVE_LIBSEQ #else */
 #endif /* USE_PTYS */
